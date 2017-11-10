e6caca74d9f788815712eb4f7a960c71bc7f8384
Villy Siu <villysiu@gmail.com>
2017-11-10 01:55:47 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,14 @@ class SessionsController < ApplicationController
   def create
     if params[:name].nil? || params[:name].empty?
 
-      redirect_to (controller: 'sessions', action: 'new')
+      redirect_to controller: 'sessions', action: 'new'
     else
       session[:name] = params[:name]
-      redirect_to (controller: 'application', action: 'hello')
+      redirect_to controller: 'application', action: 'hello'
     end
   end
   def destroy
     session.delete :name
+    redirect_to controller: 'sessions', action: 'new'
   end
 end
