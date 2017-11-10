054862ed3c0e272afbb3a0116beda23badd606ce
Villy Siu <villysiu@gmail.com>
2017-11-10 01:46:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
+    
+  end
+  def create
+    session[:name] = params[:name]
+        redirect_to '/'
+  end
+  def destroy
+    session.delete :name
   end
 end
