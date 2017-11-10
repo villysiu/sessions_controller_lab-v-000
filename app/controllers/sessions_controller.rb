cf082f9b91530e27c835b7996f55ef624c1c149a
Villy Siu <villysiu@gmail.com>
2017-11-10 01:52:10 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
   def create
     if params[:name].nil? || params[:name].empty?
-      
+
       redirect_to 'sessions/new'
     else
       session[:name] = params[:name]
